23290feec073e5b76009b13479d6632919d64dbc
Anushri <sastikaranushri@gmail.com>
2025-11-06 12:19:22 -0500

    exercise-1 without max length

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,45 @@
+
+# // Did this code successfully run on Leetcode : Yes on Geeksforgeeks
+# // Any problem you faced while coding this : Yes
+
+
+# // Your code here along with comments explaining your approach
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+          self.stacklist = []
+
+# // Time Complexity : O(1)
+# // Space Complexity :          
      def isEmpty(self):
-         
+          return not self.stacklist
+
+# // Time Complexity : O(1)
+# // Space Complexity : O(n)           
      def push(self, item):
-         
+          self.stacklist.append(item)
+
+# // Time Complexity : O(1)
+# // Space Complexity :                 
      def pop(self):
-        
-        
+          return self.stacklist.pop()
+
+# // Time Complexity : O(1)
+# // Space Complexity :           
      def peek(self):
-        
+          return self.stacklist[-1]
+
+# // Time Complexity : O(1)
+# // Space Complexity :           
      def size(self):
-         
+          return len(self.stacklist)
+
+# // Time Complexity : O(1)
+# // Space Complexity :         
      def show(self):
+          return self.stacklist
          
 
 s = myStack()
