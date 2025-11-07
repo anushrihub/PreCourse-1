72ce675fc988182a86bad9dc0a3e70309e5ff22b
Anushri <sastikaranushri@gmail.com>
2025-11-07 18:19:28 -0500

    code working for push and not for pop

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,31 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.current_node = None
+        self.previous_node = None
+
     def push(self, data):
+        if self.current_node:
+            self.previous_node = self.current_node
+        
+        self.current_node = Node(data)
+
+        if self.previous_node:
+            self.previous_node.next = self.current_node
+
         
     def pop(self):
+        if self.current_node:
+            popped = self.current_node
+            print(self.previous_node.data)
+            if self.previous_node:
+                self.current_node = self.previous_node
+                self.previous_node = None
+            else:
+                self.current_node = None
+            return popped.data
+        return None
+
         
 a_stack = Stack()
 while True:
